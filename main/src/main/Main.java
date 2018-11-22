d617c78f1cc42a92ab0c84b234844b1ca87ede22
root <root@minerd>
2018-11-21 22:31:08 -0400

    Prueba 2

--- main/src/main/Main.java
+++ main/src/main/Main.java
@@ -18,6 +18,7 @@ public class Main {
      */
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"Hola");
+        JOptionPane.showMessageDialog(null,"Hola asdad");
     }
     
 }
